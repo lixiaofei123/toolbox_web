2b107c5806b9ec1d9483b672fb2eaa70499c3f64
lixiaofei123 <326256365@qq.com>
2025-09-08 05:45:03 +0000

    debug

--- functions/api/dwz/index.ts
+++ functions/api/dwz/index.ts
@@ -11,38 +11,53 @@ function generateRandomString(length: number = 6): string {
 
 // 创建新的短网址
 export async function onRequestPut({ request, params, env }) {
-    const body = request.json()
-    const url = body.url
-    const password = body.password
-    const length = body.length || 6
-    if (env.password) {
-        if (password !== password) {
-            return new Response(JSON.stringify({ data: '密码错误' }), {
-                status: 401,
-                headers: {
-                    'content-type': 'application/json; charset=UTF-8',
-                    'Access-Control-Allow-Origin': '*',
-                },
-            });
+    try {
+        const body = request.json()
+        const url = body.url
+        const password = body.password
+        const length = body.length || 6
+        if (env.password) {
+            if (env.password !== password) {
+                return new Response(JSON.stringify({ data: '密码错误' }), {
+                    status: 401,
+                    headers: {
+                        'content-type': 'application/json; charset=UTF-8',
+                        'Access-Control-Allow-Origin': '*',
+                    },
+                });
+            }
         }
-    }
 
-    let key = generateRandomString(length)
-    while (true) { 
-        let value = await dwz.get(key);
-        if (value === null) {
-            await dwz.put(key, url);
-            break
+        let key = generateRandomString(length)
+        while (true) {
+            let value = await dwz.get(key);
+            if (value === null) {
+                await dwz.put(key, url);
+                break
+            }
+            key = generateRandomString(length)
         }
-        key = generateRandomString(length)
-    }
 
-    return new Response(JSON.stringify({ data: key }), {
-        headers: {
-            'content-type': 'application/json; charset=UTF-8',
-            'Access-Control-Allow-Origin': '*',
-        },
-    });
+        return new Response(JSON.stringify({ data: key }), {
+            headers: {
+                'content-type': 'application/json; charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    } catch (error: unknown) {
+        let message = error
+        if (error instanceof Error) {
+            message = error.message
+        }
+            
+         return new Response(JSON.stringify({ data: message }), {
+            status: 504,
+            headers: {
+                'content-type': 'application/json; charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    }
 }
 
 
